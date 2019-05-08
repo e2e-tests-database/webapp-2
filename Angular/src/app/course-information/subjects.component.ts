b1ef6857e5bcef31e29718a16f5ac5329c3618d2
AnonymusDeveloper2019 <anonymus.developer2019@gmail.com>
2019-05-08 11:47:53 +0200

    Commit initial, this application contains a backend in spring, a frontend in angular and tests e2e

---
+++ Angular/src/app/course-information/subjects.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CourseService } from './course.service';
+import { User } from '../model/user.model';
+import { Subject } from '../model/subject.model';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-subjects',
+  templateUrl: './subjects.component.html',
+  styleUrls: ['./clean-blog.component.css']
+})
+
+export class SubjectsComponent implements OnInit {
+  user: User;
+  subjects: Subject[];
+  courseID: number;
+  url: string = environment.URL;
+
+  constructor(private router: Router, private courseService: CourseService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.courseID = this.activatedRoute.snapshot.params['id'];
+    this.courseService.subjects(this.courseID).subscribe(subjects => this.subjects = subjects,
+        error => console.error(error)
+    );
+  }
+}
+
